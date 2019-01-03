297eccf667ba0621c3536c5106a24875d10b5dfb
M'LIK Mahdi <mlikmahdi1@gmail.com>
2019-01-03 12:41:41 +0100

    test codacy

--- Unsecure-master/src/UnsecureBundle/Controller/AdminController.php
+++ Unsecure-master/src/UnsecureBundle/Controller/AdminController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdminController extends Controller
 {
+    /* index action */
     public function indexAction()
     {
         $user = $this->get('unsecure.session')->getUser();
